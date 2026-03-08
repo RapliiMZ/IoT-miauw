c110219ab4b4d100a56ec2a772bef21d8c1332c3
RapliiMZ <rm.zaky97@gmail.com>
2026-03-08 19:54:03 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const MQTT_CONFIG = {
+  host: "iot.midragondev.my.id",
+  port: 9001, // Tetap gunakan port WebSocket
+  user: "nexaryn",
+  pass: "31750321",
+  clientId: "WEB_READER_" + Math.random().toString(16).substr(2, 5),
+};
+
+const client = new Paho.MQTT.Client(
+  MQTT_CONFIG.host,
+  Number(MQTT_CONFIG.port),
+  MQTT_CONFIG.clientId
+);
+
+function connectMQTT() {
+  client.connect({
+    userName: MQTT_CONFIG.user,
+    password: MQTT_CONFIG.pass,
+    onSuccess: () => {
+      console.log("Connected to Sensor Feed");
+      updateNavStatus(true);
+      client.subscribe("MIAUW/sensor/distance"); // Subscribe ke data sensor
+    },
+    onFailure: (err) => {
+      console.log("Failed: " + err.errorMessage);
+      updateNavStatus(false);
+    },
+  });
+}
+
+// Menangani data yang masuk
+client.onMessageArrived = (message) => {
+  if (message.destinationName === "MIAUW/sensor/distance") {
+    const val = message.payloadString;
+    document.getElementById("distance-value").innerText = val;
+
+    const statusText = document.getElementById("distance-status");
+    const buzzerIndicator = document.getElementById("buzzer-indicator");
+    const buzzerText = document.getElementById("buzzer-text");
+    const dist = parseFloat(val);
+
+    if (dist < 5) {
+      statusText.innerText = "Objek Terlalu Dekat!";
+      statusText.style.color = "#ef4444";
+
+      // Update UI Buzzer Aktif
+      buzzerIndicator.className = "buzzer-status active-alarm";
+      buzzerText.innerText = "ON (ALARM)";
+    } else {
+      statusText.innerText = "Aman";
+      statusText.style.color = "#22c55e";
+
+      // Update UI Buzzer Mati
+      buzzerIndicator.className = "buzzer-status inactive";
+      buzzerText.innerText = "OFF";
+    }
+  }
+};
+
+function updateNavStatus(status) {
+  const box = document.getElementById("connection-status");
+  document.getElementById("status-text").innerText = status
+    ? "Connected"
+    : "Disconnected";
+  box.className = status ? "status-box connected" : "status-box disconnected";
+}
+
+window.onload = connectMQTT;
